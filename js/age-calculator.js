30dc37448cee57894958e899346e557265207ba1
Sarah Lindmar <srhcrete@gmail.com>
2017-10-13 11:56:22 -0700

    refactor object classes

--- js/age-calculator.js
+++ js/age-calculator.js
@@ -1,4 +1,6 @@
-export class Calculator {
+var moment = require('moment');
+
+export class Age {
   constructor(name, age) {
     this.age = age;
     this.name = name;
@@ -6,6 +8,33 @@ export class Calculator {
 
   ageInSeconds(age) {
     var secondsAge = parseInt(age) * 365 * 24 * 60 * 60;
-    return secondsAge;
+    return secondsAge + "seconds";
+  }
+}
+
+export class Date {
+  constructor(year, month, date) {
+    this.year = year;
+    this.month = month;
+    this.date = date;
+  }
+
+  ageYears() {
+    return moment(`${this.year}${this.month}${this.day}`, "YYYYMMDD").fromNow();
+  }
+}
+
+export class Calculator {
+  constructor(date1, date2) {
+    this.date1 = date1;
+    this.date2 = date2;
+}
+
+  difInYears(date1, date2) {
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+    var difference_ms = (date2ms - date1ms)/1000;
+    var seconds = Math.floor(difference_ms % 60);
+    return seconds + "seconds";
   }
 }
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,1 +1,9 @@
-import { Calculator } from './../js/age-calculator.js';
+import { Calculator, Date } from './../js/age-calculator.js';
+
+
+// document.getElementById("dateInput").addEventListener("change", function() {
+//     var input = this.value;
+//     var dateEntered = new Date(input);
+//     console.log(input); //e.g. 2015-11-13
+//     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+// });
